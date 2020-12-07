ac9daafc51e4cba6b8a92625a14b6a864d47a5ce
Cao Anh Nhat <an.dev2504@gmail.com>
2020-12-07 08:48:20 +0700

    add env

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,9 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 
+// Local import
+import { I18N_DEFAULT_LNG } from '@definitions/config';
+
 i18n
   .use(Backend)
   .use(initReactI18next)
@@ -9,6 +12,6 @@ i18n
     backend: {
       loadPath: '/locales/{{lng}}/{{ns}}.json',
     },
-    lng: 'jp',
+    lng: I18N_DEFAULT_LNG,
   });
 export default i18n;
--- src/shared/definitions/config/index.ts
+++ src/shared/definitions/config/index.ts
@@ -1,1 +1,2 @@
 export const BASE_API_URL = process.env.REACT_APP_BASE_URL_API || 'http://localhost:3000';
+export const I18N_DEFAULT_LNG = process.env.REACT_APP_I18N_DEFAULT_LNG || 'jp';
